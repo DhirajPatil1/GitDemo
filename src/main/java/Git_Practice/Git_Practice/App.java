11bf198f3faaa7f137d7d0f72c33c9f8da9fb155
DHIRAJ JAGATRAO PATIL <dhiraj.patil@cactusglobal.com>
2022-04-21 13:16:47 +0530

    add project

---
+++ src/main/java/Git_Practice/Git_Practice/App.java
@@ -0,0 +1,13 @@
+package Git_Practice.Git_Practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
