6caaaf860d93962f15ce2557611b0b15cdc5a860
DHIRAJ JAGATRAO PATIL <dhiraj.patil@cactusglobal.com>
2022-04-22 08:29:02 +0530

    Added new file

---
+++ src/test/java/Git_Practice/Git_Practice/Test.java
@@ -0,0 +1,11 @@
+package Git_Practice.Git_Practice;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("inside Test of p1");
+	}
+
+}
