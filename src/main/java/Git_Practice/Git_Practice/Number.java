9f2ca4046a52c0b81d10bb79088a153cc08c25d2
Dhiraj <dhiraj.patil@cactusglobal.com>
2022-09-15 11:57:39 +0530

    update

--- src/main/java/Git_Practice/Git_Practice/Number.java
+++ src/main/java/Git_Practice/Git_Practice/Number.java
@@ -4,8 +4,11 @@ public class Number {
 
 	public void testOne() {
 		for(int i=0; i<5;i++) {
+			System.out.println(i+1);
 			System.out.println(i);
-			System.out.println(i);
+			System.out.println("Dhiraj");
+			System.out.println("Ramesh");
+
 		}
 	}
 }
