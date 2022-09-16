cc3c0df19ab545244240e933fe591ae4c81bd620
Dhiraj <dhiraj.patil@cactusglobal.com>
2022-09-16 13:52:07 +0530

    added new file

---
+++ src/test/java/Git_Practice/Git_Practice/Test1.java
@@ -0,0 +1,10 @@
+package Git_Practice.Git_Practice;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Demo file");
+	}
+
+}
